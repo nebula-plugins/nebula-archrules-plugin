4f177fef67acbcc79cfb0427723e519bccf73733
John Burns <wakingrufus@gmail.com>
2025-10-23 10:22:39 -0500

    add empty gradle build and standard github actions

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL // ALL helps when debugging gradle plugins
+    gradleVersion = "9.1.0"
+    distributionSha256Sum = "b84e04fa845fecba48551f425957641074fcc00a88a84d2aae5808743b35fc85"
+}
