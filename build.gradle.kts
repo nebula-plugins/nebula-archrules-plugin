7b1120cb9d49972fc7b076f05859f61af0bb5966
John Burns <wakingrufus@gmail.com>
2025-11-01 13:45:13 -0500

    add nebula plugins to enable publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
-
+plugins {
+    id("com.netflix.nebula.root")
+}
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL // ALL helps when debugging gradle plugins
     gradleVersion = "9.1.0"
     distributionSha256Sum = "b84e04fa845fecba48551f425957641074fcc00a88a84d2aae5808743b35fc85"
+}
+dependencyLocking {
+    lockAllConfigurations()
+}
+contacts {
+    addPerson("nebula-plugins-oss@netflix.com") {
+        moniker = "Nebula Plugins Maintainers"
+        github = "nebula-plugins"
+    }
 }
---
+++ nebula-archrules-gradle-plugin/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.netflix.nebula.plugin-plugin")
+    `kotlin-dsl`
+}
+description = "Plugins for authoring and running Nebula ArchRules"
+repositories {
+    mavenCentral()
+}
+gradlePlugin {
+    plugins {
+        create("library") {
+            id = "com.netflix.nebula.archrules.library"
+            implementationClass = "com.netflix.nebula.archrules.gradle.ArchrulesLibraryPlugin"
+            displayName = "ArchRules Library Plugin"
+            description = "Sets up a project for declaring archrules to be used in another project via the runner plugin"
+            tags.addAll("nebula", "archunit")
+        }
+        create("runner") {
+            id = "com.netflix.nebula.archrules.runner"
+            implementationClass = "com.netflix.nebula.archrules.gradle.ArchrulesRunnerPlugin"
+            displayName = "ArchRules Runner Plugin"
+            description = "Sets up a project to consume archrules libraries and run them against the code in the current project"
+            tags.addAll("nebula", "archunit")
+        }
+    }
+}
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(11)
+    }
+}
+testing {
+    suites{
+        named<JvmTestSuite>("test"){
+            useJUnitJupiter()
+        }
+    }
+}
+dependencyLocking {
+    lockAllConfigurations()
+}
