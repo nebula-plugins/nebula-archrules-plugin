369cef70ef943a281d90438003fbd4853b0bfcf5
Emily Yuan <eyuan@netflix.com>
2025-12-08 15:26:47 -0800

    Ensure packages are added in runner checker
    fix package loading to ensure only package-info is loaded, not the whole package
    
    Co-authored-by: John Burns <wakingrufus@gmail.com>

--- nebula-archrules-core/src/main/java/com/netflix/nebula/archrules/core/Runner.java
+++ nebula-archrules-core/src/main/java/com/netflix/nebula/archrules/core/Runner.java
@@ -2,11 +2,27 @@ package com.netflix.nebula.archrules.core;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.Location;
+import com.tngtech.archunit.core.importer.Locations;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.EvaluationResult;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Runner {
+    /**
+     * Check a rule against some classes.
+     * This can be invoked from the real Gradle plugin or unit tests for rules to ensure the same logic is observed there.
+     *
+     * @param rule           the rule to run
+     * @param classesToCheck the classes to run the rule against
+     * @return the result, which contains information about failure
+     */
     public static EvaluationResult check(ArchRule rule, JavaClasses classesToCheck) {
         try {
             return rule.evaluate(classesToCheck);
@@ -24,16 +40,35 @@ public class Runner {
     }
 
     /**
-     * Check a rule against some classes.
-     * This can be invoked from the real Gradle plugin or unit tests for rules to ensure the same logic is observed there.
+     * Check a rule against some classes and their package-info
+     * This intended to be used from unit tests for rules to ensure the same logic is observed there.
      *
      * @param rule           the rule to run
      * @param classesToCheck the classes to run the rule against
      * @return the result, which contains information about failure
      */
     public static EvaluationResult check(ArchRule rule, Class<?>... classesToCheck) {
+        Set<Location> locs = Arrays.stream(classesToCheck)
+                .map(Locations::ofClass)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+        List<URL> uris = Arrays.stream(classesToCheck)
+                .map(clazz -> clazz.getPackage().getName())
+                .map(Locations::ofPackage)
+                .flatMap(it -> it.stream().map(Location::asURI))
+                .map(u -> URI.create(u.toASCIIString() + "package-info.class"))
+                .map(uri -> {
+                    try {
+                        return uri.toURL();
+                    } catch (MalformedURLException e) {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        locs.addAll(Locations.of(uris));
         final JavaClasses classes = new ClassFileImporter()
-                .importClasses(classesToCheck);
+                .importLocations(locs);
         return check(rule, classes);
     }
 }
---
+++ nebula-archrules-core/src/test/java/com/netflix/nebula/archrules/testpackage/package-info.java
@@ -0,0 +1,2 @@
+@Deprecated
+package com.netflix.nebula.archrules.testpackage;
