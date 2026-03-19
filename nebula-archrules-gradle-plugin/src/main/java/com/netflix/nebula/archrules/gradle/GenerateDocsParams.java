8e34a8c8f307d95d14dc78aaa06c33c58a045612
Emily Yuan <57610631+emilyyuan03@users.noreply.github.com>
2026-03-19 14:39:51 -0700

    Refactor GenerateRulesDocumentation task to use work action (#69)

---
+++ nebula-archrules-gradle-plugin/src/main/java/com/netflix/nebula/archrules/gradle/GenerateDocsParams.java
@@ -0,0 +1,15 @@
+package com.netflix.nebula.archrules.gradle;
+
+import org.gradle.api.provider.ListProperty;
+import org.gradle.api.provider.Property;
+import org.gradle.workers.WorkParameters;
+import org.jspecify.annotations.NullMarked;
+
+import java.io.File;
+
+@NullMarked
+public interface GenerateDocsParams extends WorkParameters {
+    ListProperty<String> getOwnArchRulesClasses();
+
+    Property<File> getOutputFile();
+}
--- nebula-archrules-gradle-plugin/src/main/kotlin/com/netflix/nebula/archrules/gradle/GenerateRulesDocumentationTask.kt
+++ nebula-archrules-gradle-plugin/src/main/kotlin/com/netflix/nebula/archrules/gradle/GenerateRulesDocumentationTask.kt
@@ -1,9 +1,5 @@
 package com.netflix.nebula.archrules.gradle
 
-import com.netflix.nebula.archrules.core.ArchRulesService
-import com.netflix.nebula.archrules.core.Runner
-import com.tngtech.archunit.lang.ArchRule
-import com.tngtech.archunit.lang.Priority
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
@@ -11,12 +7,14 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Classpath
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.submit
+import org.gradle.workers.WorkQueue
+import org.gradle.workers.WorkerExecutor
 import java.io.File
-import java.net.URLClassLoader
-import java.util.ServiceLoader
+import javax.inject.Inject
 
 @CacheableTask
-abstract class GenerateRulesDocumentationTask : DefaultTask() {
+abstract class GenerateRulesDocumentationTask @Inject constructor(private val workerExecutor: WorkerExecutor) : DefaultTask() {
 
     @get:Classpath
     abstract val rulesClasspath: ConfigurableFileCollection
@@ -26,9 +24,6 @@ abstract class GenerateRulesDocumentationTask : DefaultTask() {
 
     @TaskAction
     fun generateDocs() {
-        val rules = mutableListOf<RuleMetadata>()
-        val classpathUrls = rulesClasspath.files.map { it.toURI().toURL() }.toTypedArray()
-
         val ownArchRulesClasses = rulesClasspath.files.map {
             File(
                 it,
@@ -41,56 +36,13 @@ abstract class GenerateRulesDocumentationTask : DefaultTask() {
             ?.toSet()
             ?: emptySet()
 
-        URLClassLoader(classpathUrls, this.javaClass.classLoader).use { classLoader ->
-            ServiceLoader.load(ArchRulesService::class.java, classLoader)
-                .filter { it.javaClass.name in ownArchRulesClasses }
-                .forEach { service ->
-                val serviceClassName = service.javaClass.name
-                service.rules.forEach { (ruleName, archRule) ->
-                    rules.add(
-                        RuleMetadata(
-                            ruleClass = serviceClassName,
-                            ruleName = ruleName,
-                            description = archRule.description,
-                            priority = getPriority(archRule)
-                        )
-                    )
-                }
-            }
+        val workQueue: WorkQueue = workerExecutor.classLoaderIsolation {
+            classpath.from(rulesClasspath)
         }
 
-        val output = outputFile.get().asFile
-        output.parentFile.mkdirs()
-        output.writeText(formatMarkdown(rules))
-    }
-
-    private fun formatMarkdown(rules: List<RuleMetadata>): String {
-        val str = StringBuilder()
-        str.append("# ArchRules Documentation\n\n")
-        str.append("List of all archrules defined in this library.\n\n")
-
-        val sortedRulesByName = rules.sortedBy { it.ruleName }
-        sortedRulesByName.forEach { rule ->
-            str.append("## ${rule.ruleName}\n\n")
-            str.append("**Description:** ${rule.description}\n\n")
-            str.append("**Priority:** ${rule.priority}\n\n")
-            str.append("**Class:** `${rule.ruleClass}`\n\n")
-            str.append("---\n\n")
+        workQueue.submit(GenerateDocsWorkAction::class) {
+            getOwnArchRulesClasses().set(ownArchRulesClasses)
+            getOutputFile().set(this@GenerateRulesDocumentationTask.outputFile.get().asFile)
         }
-
-        return str.toString()
     }
-
-    // workaround since ArchRule priority is private
-    private fun getPriority(archRule: ArchRule): Priority {
-        val dummyResult = Runner.check(archRule)
-        return dummyResult.priority
-    }
-
-    private data class RuleMetadata(
-        val ruleClass: String,
-        val ruleName: String,
-        val description: String,
-        val priority: Priority
-    )
 }
